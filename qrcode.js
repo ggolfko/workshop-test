deb91795bfaf6a75a71e6df2ab250d5b72d8e99d
Kitti Onthong <5535512134@email.psu.ac.th>
2018-01-10 17:47:27 +0700

    Added commit

--- qrcode.js
+++ qrcode.js
@@ -1,3 +1,60 @@
+const https = require('https')
+const fs = require('fs')
+const querystring = require('querystring')
+
+let config = {
+  host: 'https://api.beecard.net:81/',
+  path: 'ebrochures?',
+  ebrochureId: 'id=591b90d3-12d0-46d4-9ce0-b985874723c8',
+  logo: 'https://static-unitag.com/file/freeqr/f5a47ac99927d7c91dedfe78983471a4.png'
+}
+
+let url = config.host + config.path + config.ebrochureId
+
+let data = {
+  data: '{"TYPE":"url","DATA":{"URL":"' + url + '"}}',
+  setting: '{"EYES":{"EYE_TYPE":"Spike"},"E":"L","BODY_TYPE":5,"LAYOUT":{"COLORBG":"ffffff","GRADIENT_TYPE":"NO_GR","COLOR1":"000000"},"ARRONDI":3,"LOGO":{"L_NAME":"' + config.logo + '","EXCAVATE":false}}'
+}
+
+const options = {
+  hostname: 'unitag-qr-code-generation.p.mashape.com',
+  port: 443,
+  path: '/api?' + querystring.stringify(data),
+  method: 'GET',
+  headers: {
+    'X-Mashape-Key': 'HPVJbAiFWTmshKVnclvt0O0gJjhmp1FamVCjsnBcT3tHzBlvaP',
+  }
+}
+
+function base64_encode(file) {
+  // read binary data
+  // var bitmap = fs.readFileSync(file);
+  // convert binary data to base64 encoded string
+  return new Buffer(file, 'binary').toString('base64');
+}
+
+var req = https.request(options, (res) => {
+  var body = '';
+  res.setEncoding('binary')
+  res.on('data', (chunk) => {
+    body += chunk
+  })
+  res.on('end', () => {
+    var base64str = base64_encode(body);
+    console.log("data:image/png;base64,%s", base64str)
+
+    // fs.writeFile("test.png", body, 'binary', (err) => {
+    //   if (err) {
+    //     return console.log(err);
+    //   }
+
+    // })
+  })
+})
+
+req.end();
+
+
 // var QRCode = require('qrcode')
 
 // QRCode.toDataURL('http://www.google.com', {
@@ -20,13 +77,3 @@
 // qrCodeLogo(url, qrcodeImgFilePath, function (err, img) {
 //   console.log(err, img); // img为生成二维码图片信息（包含高度、宽度等信息） 
 // });
-
-var fancyQR = require('fancyqr');
-
-fancyQR.toDataURL('hello world!', {
-  logoPath: __dirname + '/logo.png'
-}, function (err, urlString) {
-  if (err) console.log(err)
-  console.log(urlString)
-  //do something here
-});
