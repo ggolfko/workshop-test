13b6171461feb978939bed9a62d4ece8454d8f2a
Kitti Onthong <5535512134@email.psu.ac.th>
2018-01-12 14:37:32 +0700

    Added jimp

--- jimp.js
+++ jimp.js
@@ -1,10 +1,14 @@
 const Jimp = require("jimp")
+const del = require("del")
 
-Jimp.read("images/o/Capture4.PNG").then(function (lenna) {
-  lenna.resize(2048, 2732) // resize
+Jimp.read("images/o/Capture.jpg").then(function (lenna) {
+  lenna.resize(200, 200) // resize
     .quality(60) // set JPEG quality
     // .greyscale() // set greyscale
-    .write("images/n/Capture4.jpg") // save
+    .write("images/n/Capture.jpg") // save
+
+  del.sync([`images/o/Capture.jpg`, `!/images/o`])
+
 }).catch(function (err) {
   console.error(err)
-});
+})
