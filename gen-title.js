9acb7abed193006240055383db57e11a175539b2
Kitti Onthong <5535512134@email.psu.ac.th>
2018-01-14 01:31:30 +0700

    Added gen-title-url

---
+++ gen-title.js
@@ -0,0 +1,7 @@
+let text = 'ฟหกฟหก ขหหกด หกด asdasdasa asdasd'
+text = text.toLocaleLowerCase().replace(/\s+/g, '-')
+var id = function () {
+  return '-' + (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toLocaleLowerCase()
+};
+let title = text + id()
+console.log(title)
