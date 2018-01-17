0711b5572721f4961f15cc1ec5752fca73e39937
Kitti Onthong <5535512134@email.psu.ac.th>
2018-01-17 16:52:58 +0700

    Updated resize jimp

--- jimp.js
+++ jimp.js
@@ -1,8 +1,8 @@
 const Jimp = require("jimp")
 const del = require("del")
 
-Jimp.read("images/o/Capture.jpg").then(function (lenna) {
-  lenna.resize(200, 200) // resize
+Jimp.read("images/o/Capture.PNG").then(function (lenna) {
+  lenna.resize(1000, 1000) // resize
     .quality(60) // set JPEG quality
     // .greyscale() // set greyscale
     .write("images/n/Capture.jpg") // save
