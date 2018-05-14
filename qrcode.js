6f4b954398ef94bcf61242e9d0729d62c7d228f9
Kitti Onthong <5535512134@psu.ac.th>
2018-05-14 13:29:52 +0700

    Updated

--- qrcode.js
+++ qrcode.js
@@ -1,61 +1,61 @@
-const https = require('https')
-const fs = require('fs')
-const querystring = require('querystring')
+// const https = require('https')
+// const fs = require('fs')
+// const querystring = require('querystring')
 
-let config = {
-  host: 'https://api.beecard.net:81/',
-  path: 'ebrochures?',
-  ebrochureId: 'id=591b90d3-12d0-46d4-9ce0-b985874723c8',
-  logo: 'https://static-unitag.com/file/freeqr/f5a47ac99927d7c91dedfe78983471a4.png'
-}
+// let config = {
+//   host: 'https://api.beecard.net:81/',
+//   path: 'ebrochures?',
+//   ebrochureId: 'id=591b90d3-12d0-46d4-9ce0-b985874723c8',
+//   logo: 'https://static-unitag.com/file/freeqr/f5a47ac99927d7c91dedfe78983471a4.png'
+// }
 
-let url = config.host + config.path + config.ebrochureId
+// let url = config.host + config.path + config.ebrochureId
 
-let data = {
-  data: '{"TYPE":"url","DATA":{"URL":"' + url + '"}}',
-  setting: '{"EYES":{"EYE_TYPE":"Spike"},"E":"L","BODY_TYPE":5,"LAYOUT":{"COLORBG":"ffffff","GRADIENT_TYPE":"NO_GR","COLOR1":"000000"},"ARRONDI":3,"LOGO":{"L_NAME":"' + config.logo + '","EXCAVATE":false}}'
-}
+// let data = {
+//   data: '{"TYPE":"url","DATA":{"URL":"' + url + '"}}',
+//   setting: '{"EYES":{"EYE_TYPE":"Spike"},"E":"L","BODY_TYPE":5,"LAYOUT":{"COLORBG":"ffffff","GRADIENT_TYPE":"NO_GR","COLOR1":"000000"},"ARRONDI":3,"LOGO":{"L_NAME":"' + config.logo + '","EXCAVATE":false}}'
+// }
 
-const options = {
-  hostname: 'unitag-qr-code-generation.p.mashape.com',
-  port: 443,
-  path: '/api?' + querystring.stringify(data),
-  method: 'GET',
-  headers: {
-    'X-Mashape-Key': 'HPVJbAiFWTmshKVnclvt0O0gJjhmp1FamVCjsnBcT3tHzBlvaP',
-  }
-}
+// const options = {
+//   hostname: 'unitag-qr-code-generation.p.mashape.com',
+//   port: 443,
+//   path: '/api?' + querystring.stringify(data),
+//   method: 'GET',
+//   headers: {
+//     'X-Mashape-Key': 'HPVJbAiFWTmshKVnclvt0O0gJjhmp1FamVCjsnBcT3tHzBlvaP',
+//   }
+// }
 
-function base64_encode(file) {
-  // read binary data
-  // var bitmap = fs.readFileSync(file);
-  // convert binary data to base64 encoded string
-  return new Buffer(file, 'binary').toString('base64');
-}
+// function base64_encode(file) {
+//   // read binary data
+//   // var bitmap = fs.readFileSync(file);
+//   // convert binary data to base64 encoded string
+//   return new Buffer(file, 'binary').toString('base64');
+// }
 
-var req = https.request(options, (res) => {
-  var body = '';
-  res.setEncoding('binary')
-  res.on('data', (chunk) => {
-    body += chunk
-  })
-  res.on('end', () => {
-    var base64str = base64_encode(body);
-    console.log("data:image/png;base64,%s", base64str)
+// var req = https.request(options, (res) => {
+//   var body = '';
+//   res.setEncoding('binary')
+//   res.on('data', (chunk) => {
+//     body += chunk
+//   })
+//   res.on('end', () => {
+//     var base64str = base64_encode(body);
+//     console.log("data:image/png;base64,%s", base64str)
 
-    // fs.writeFile("test.png", body, 'binary', (err) => {
-    //   if (err) {
-    //     return console.log(err);
-    //   }
+//     // fs.writeFile("test.png", body, 'binary', (err) => {
+//     //   if (err) {
+//     //     return console.log(err);
+//     //   }
 
-    // })
-  })
-})
+//     // })
+//   })
+// })
 
-req.end();
+// req.end();
 
 
-// var QRCode = require('qrcode')
+var QRCode = require('qrcode')
 
 // QRCode.toDataURL('http://www.google.com', {
 //   color: {
@@ -66,6 +66,25 @@ req.end();
 //   if (err) throw err
 //   console.log(string)
 // })
+
+var opts = {
+  errorCorrectionLevel: 'H',
+  type: 'image/png',
+  rendererOpts: {
+    quality: 0.3
+  },
+  color: {
+    dark: '#000', // Blue dots
+    // light: '#0000' // Transparent background
+  },
+  width: 1000
+}
+
+QRCode.toFile('filename.png', 'Some text', opts, function (err) {
+  if (err) throw err
+  console.log('done')
+})
+
 
 
 // var fs = require('fs'),
